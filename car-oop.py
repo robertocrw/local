4859a004860f722c0f2811d40c2e4860c4afceb7
Roberto Pinto <robertowashington.bailao@gmail.com>
2015-01-03 23:55:56 +0000

    OOP python object car

---
+++ car-oop.py
@@ -0,0 +1,24 @@
+class car:
+	def _init_(self, gasolina):
+		self.gasolina = gasolina
+	def arrancar(self):
+	     if self.gasolina > 0:
+	         print('Arrancar')
+	     else: 
+	         print('Não liga')
+	def conduzir(self):
+	     if self.gasolina > 0: 
+	        self.gasolina -= 1
+	        print('Menos %i litros de gasolina' %(self.gasolina))
+	     else:
+	        print('Acabou o combustivel')
+
+opala = car(3)
+opala.arrancar()
+opala.conduzir()
+opala.arrancar()
+opala.conduzir()
+opala.arrancar()
+opala.conduzir()
+opala.arrancar()
+opala.conduzir()	                                 	
